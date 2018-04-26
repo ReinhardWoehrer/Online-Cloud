49a4f0b16824afe8ee769321f9f7b4f0745fde1b
DomKme <37835915+DomKme@users.noreply.github.com>
2018-04-26 11:04:22 +0200

    Add files via upload

---
+++ Webseite/content/navbar.php
@@ -0,0 +1,20 @@
+	<div class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+						<a class="navbar-brand" href="#">Online-Cloud</a>
+						<button class="btn navbar-toggle" data-toggle="collapse" data-target="#navlist" type="button" aria-expanded="false" aria-controls="navlist">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+			</div>
+			<div id="navlist" class="collapse navbar-collapse">
+				<ul  class="nav navbar-nav navbar-right" >
+					<li><a href="Main_Page.php">Home</a></li> <!-- if ?section=home isnt there homepage wont load correctly -->
+					<li><a href="Main_Page.php?section=settings">Settings</a></li>
+					<li><a href="Main_Page.php?section=news">News</a></li>
+					
+				</ul>
+			</div>
+		</div>
+	</div>
